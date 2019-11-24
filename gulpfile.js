34f243522d5b4f3d1ec5a5a9de42449d70897534
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2019-11-24 21:52:55 +0300

    v.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -68,6 +68,7 @@ gulp.task('sass', function(){
 gulp.task('js', function() {
 	return gulp.src([
 			'src/libs/jquery/dist/jquery.min.js',
+			'src/libs/slick-1.8.1/slick/slick.min.js',
 			'src/js/common.js', // Always at the end
 			])
 		//.pipe(concat('scripts.min.js'))
