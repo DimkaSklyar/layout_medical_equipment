0b4c6d758671cc7b2149539dc975e750cd9cadf2
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2020-04-04 18:28:31 +0300

    v.0.5.0

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   function setCursorPosition(pos, elem) {
     elem.focus();
     if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
@@ -10,14 +10,14 @@ window.addEventListener("DOMContentLoaded", function() {
       range.select()
     }
   }
-  
+
   function mask(event) {
     var matrix = "+7 (___) ___ __ __",
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
-    val = this.value.replace(/\D/g, "");
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
     if (def.length >= val.length) val = def;
-    this.value = matrix.replace(/./g, function(a) {
+    this.value = matrix.replace(/./g, function (a) {
       return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
     });
     if (event.type == "blur") {
@@ -25,6 +25,10 @@ window.addEventListener("DOMContentLoaded", function() {
     } else setCursorPosition(this.value.length, this)
   };
   var input2 = document.querySelector("#phone-1");
+  input2.addEventListener("input", mask, false);
+  input2.addEventListener("focus", mask, false);
+  input2.addEventListener("blur", mask, false);
+  var input2 = document.querySelector("#call-me-phone-2");
   input2.addEventListener("input", mask, false);
   input2.addEventListener("focus", mask, false);
   input2.addEventListener("blur", mask, false);
@@ -159,60 +163,60 @@ $(document).ready(function () {
   });
 
 
-$(".mobile-menu__btn").click(function (e) { 
-  e.preventDefault();
-  $(".top-menu").toggleClass("active");
-  $(".mobile-menu__close").toggleClass("active");
-});
+  $(".mobile-menu__btn").click(function (e) {
+    e.preventDefault();
+    $(".top-menu").toggleClass("active");
+    $(".mobile-menu__close").toggleClass("active");
+  });
 
-$(".mobile-menu__close").click(function (e) { 
-  e.preventDefault();
-  $(".top-menu").toggleClass("active");
-  $(".mobile-menu__close").toggleClass("active");
-});
+  $(".mobile-menu__close").click(function (e) {
+    e.preventDefault();
+    $(".top-menu").toggleClass("active");
+    $(".mobile-menu__close").toggleClass("active");
+  });
 
-$('.popup-with-move-anim').magnificPopup({
-  type: 'inline',
+  $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
 
-  fixedContentPos: false,
-  fixedBgPos: true,
+    fixedContentPos: false,
+    fixedBgPos: true,
 
-  overflowY: 'auto',
+    overflowY: 'auto',
 
-  closeBtnInside: true,
-  preloader: false,
-  
-  midClick: true,
-  removalDelay: 300,
-  mainClass: 'my-mfp-slide-bottom'
-});
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
 
 });
 var priceBegin;
 $(".main-order").click(function () {
-	$("#price").text($(this).closest(".product__item").find(".product__price").text());
-	$("#quanty").val(1);
-	$("#product-img").attr("src",  $(this).closest(".product__item").find("img").attr("src"));
-	$("#product-name").text($(this).closest(".product__item").find(".product__header").text());
+  $("#price").text($(this).closest(".product__item").find(".product__price").text());
+  $("#quanty").val(1);
+  $("#product-img").attr("src", $(this).closest(".product__item").find("img").attr("src"));
+  $("#product-name").text($(this).closest(".product__item").find(".product__header").text());
   $("#description-1").text($(this).closest(".product__item").find(".d-1").text());
   priceBegin = $('#price').text().replace(/руб./g, "");
 });
 
-$('#pay').click(function () { 
-	$('.order-form').addClass('active');
+$('#pay').click(function () {
+  $('.order-form').addClass('active');
   var price = priceBegin;
   var count = $('#quanty').val();
   var sum = +price * +count;
-	var sum2 = sum.toString().split('');
-	sum2.splice(sum2.length-3,0," ");
-	sum = sum2.join('');
-	$('#quality-order').val($('#quanty').val());
-	$('#sum').val(sum + " руб.");
-	$('#price-order').val(price);
-	$('#count-order').val(count);
-	$('#sum-order').val(sum);
-	$('#name-order').val($('#product-name').text());
-	$('.close').removeClass('close-active');
+  var sum2 = sum.toString().split('');
+  sum2.splice(sum2.length - 3, 0, " ");
+  sum = sum2.join('');
+  $('#quality-order').val($('#quanty').val());
+  $('#sum').val(sum + " руб.");
+  $('#price-order').val(price);
+  $('#count-order').val(count);
+  $('#sum-order').val(sum);
+  $('#name-order').val($('#product-name').text());
+  $('.close').removeClass('close-active');
 });
 
 $("#quanty").bind('keyup mouseup', function () {
@@ -221,18 +225,18 @@ $("#quanty").bind('keyup mouseup', function () {
 });
 
 $("#quality-order").bind('keyup mouseup', function () {
-  $("#sum").val(+priceBegin * $(this).val()+ " руб.");
+  $("#sum").val(+priceBegin * $(this).val() + " руб.");
 });
 
 
-$('.mfp-close').click(function () { 
-	$('.order-form').removeClass('active');
+$('.mfp-close').click(function () {
+  $('.order-form').removeClass('active');
 });
 
-$('.close').click(function () { 
-	$('.order-form').removeClass('active');
-	$(this).addClass('close-active');
-	
+$('.close').click(function () {
+  $('.order-form').removeClass('active');
+  $(this).addClass('close-active');
+
 });
 
 // отправка подписки
@@ -267,17 +271,23 @@ function func() {
   $("p.error").detach();
 }
 
-$('#form-main-order').submit(function(e){
+$('#form-main-order').submit(function (e) {
   e.preventDefault();
-  AjaxFormRequest('messegeResult-order','form-main-order','./order.php');
+  AjaxFormRequest('messegeResult-order', 'form-main-order', './order.php');
 });
 
-$('#subscribe-form').submit(function(e){
+$('#subscribe-form').submit(function (e) {
   e.preventDefault();
-  AjaxFormRequest('sub-result','subscribe-form','./subscribe.php');
+  AjaxFormRequest('sub-result', 'subscribe-form', './subscribe.php');
 });
 
-$('#get-in').submit(function(e){
+$('#get-in').submit(function (e) {
   e.preventDefault();
-  AjaxFormRequest('get-in_wrap','get-in','./feedback.php');
+  AjaxFormRequest('get-in_wrap', 'get-in', './feedback.php');
+});
+
+$(".navigation__item_with-sub").click(function (e) {
+  if ($(window).width() <= 992) {
+    $(".navigation__sub").fadeToggle();
+  }
 });
---
+++ src/js/mixit-up-common.js
@@ -0,0 +1,7 @@
+var count = 9;
+
+var mixer = mixitup(".products-mix", {
+  pagination: {
+    limit: count // impose a limit of 8 targets per page
+  }
+});
